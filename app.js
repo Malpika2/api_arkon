c68406b92df8d76ea82e486a9ec35d3278d19de0
Anthony <hamthony@hotmail.com>
2021-04-20 16:53:09 -0500

    Base de datos e insercion en tablas

--- app.js
+++ app.js
@@ -2,18 +2,17 @@
 const express = require('express')
 const app = express()
 const cors = require("cors");
-const http = require("http");
 
 const port = 4000
 
-const server = http.createServer(app);
+app.set('json spaces', 2);
 
 
 app.use(cors());
+app.use(express.json());
 
 app.use('',require('./src/routes'));
 
-
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`)
 })
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const { Router, response } = require("express");
 const router = new Router();
-// const mysqlConnection = require("../database");
+const mysqlConnection = require("../database");
 const bcrypt = require("bcryptjs");
 
 router.get('/', (req, res) => {
@@ -8,13 +8,17 @@ router.get('/', (req, res) => {
   })
 
 router.post("/tareas", (req, resp) => {
-    const { name, email, tipo } = req.body;
-    console.log(req.body);
+  console.log(req.body);
+    const { titulo, descripcion, minutes, seconds } = req.body;
+
+    const duracion = minutes*60 + seconds;
+
     const query =
-      "INSERT INTO tareas (x,x ,x ) VALUES (?, ?, ?)";
-    mysqlConnection.query(query, [ x, x,x ], (err, rows, fields) => {
+      "INSERT INTO arkondata (titulo,descripcion ,duracion,estatus ) VALUES (?, ?, ?,'Pendiente')";
+    mysqlConnection.query(query, [ titulo, descripcion,duracion ], (err, rows, fields) => {
+      console.log(query);
       if (!err) {
-        resp.status(201).json({ status: "201", message: "User added" });
+        resp.status(201).json({ status: "201", message: "Tarea registrada" });
       } else {
         resp.status(400).json({ messaje: err });
       }
