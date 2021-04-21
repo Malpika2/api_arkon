091e93e511c2ab4ed0bc7b3cbd24601fb82f2acf
Anthony <hamthony@hotmail.com>
2021-04-21 17:00:29 -0500

    save de position

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,22 +9,32 @@ router.get('/', (req, res) => {
 router.post("/tareas", (req, resp) => {
     const { titulo, descripcion, minutes, seconds } = req.body;
     const duracion = minutes*60 + seconds;
-    const query =
-      "INSERT INTO arkondata (titulo,descripcion ,duracion,estatus ) VALUES (?, ?, ?,'Pendiente')";
-    mysqlConnection.query(query, [ titulo, descripcion,duracion ], (err, rows, fields) => {
-      console.log(query);
+    
+    mysqlConnection.query( "SELECT MAX(id) as lastId from arkondata", (err, result, fields) => {
+      let query = '';
       if (!err) {
-        resp.status(201).json({ status: "201", message: "Tarea registrada" });
-      } else {
-        resp.status(400).json({ messaje: err });
-      }
-    });
+        const lastId = result[0].lastId;
+         query = `INSERT INTO arkondata (titulo,descripcion ,duracion,estatus, posicion ) VALUES (?, ?, ?,'Pendiente',result[0], ${lastId})`;
+        } else {
+          const lastId = 1
+           query = `INSERT INTO arkondata (titulo,descripcion ,duracion,estatus, posicion ) VALUES (?, ?, ?,'Pendiente',result[0], ${lastId})`;
+        }
+        mysqlConnection.query(query, [ titulo, descripcion,duracion ], (err, rows, fields) => {
+          if (!err) {
+            resp.status(201).json({ status: "201", message: "Tarea registrada" });
+          } else {
+            resp.status(400).json({ messaje: err });
+          }
+        });
+      });
   });
   
   router.get('/tareas', (req, resp) => {
-    mysqlConnection.query("SELECT * FROM arkondata", (err, rows, fields) => {
+
+    mysqlConnection.query("SELECT * FROM arkondata ORDER BY posicion ASC", (err, rows, fields) => {
       if(!err){
         resp.status(200).json({status:"200", data:rows})
+
       }else{
         resp.status(400).json({error:err})
       }
