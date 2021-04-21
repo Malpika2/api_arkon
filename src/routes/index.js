fa05105cd06ab402d9df92b6f2c3f12f3f32c6b8
Anthony <hamthony@hotmail.com>
2021-04-21 07:06:42 -0500

    getTareas

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,13 +6,9 @@ const bcrypt = require("bcryptjs");
 router.get('/', (req, res) => {
     res.send('Bienvenidos ArkonData')
   })
-
 router.post("/tareas", (req, resp) => {
-  console.log(req.body);
     const { titulo, descripcion, minutes, seconds } = req.body;
-
     const duracion = minutes*60 + seconds;
-
     const query =
       "INSERT INTO arkondata (titulo,descripcion ,duracion,estatus ) VALUES (?, ?, ?,'Pendiente')";
     mysqlConnection.query(query, [ titulo, descripcion,duracion ], (err, rows, fields) => {
@@ -25,4 +21,13 @@ router.post("/tareas", (req, resp) => {
     });
   });
   
+  router.get('/tareas', (req, resp) => {
+    mysqlConnection.query("SELECT * FROM arkondata", (err, rows, fields) => {
+      if(!err){
+        resp.status(200).json({status:"200", data:rows})
+      }else{
+        resp.status(400).json({error:err})
+      }
+    })
+  })
   module.exports = router;
